b8df563b93b32aabd6bc562f9f5b02726e50d8d0
Yan Li <yanli@trit.local>
2026-07-14 19:44:58 +0800

    fix: correct benchmark criterion usage (nf→whnf, remove CRT stubs import)

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,25 +1,17 @@
 module Main where
 import Criterion.Main
-import Dayan.Compute.CRT
 import Dayan.Core.Tryte (mkTryte, decode)
 import Dayan.Core.Trit
 import Data.Word
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "CRT" 
-      [ bench "lookupCrt 0"    $ nf lookupCrt 0
-      , bench "lookupCrt 3312" $ nf lookupCrt 3312
-      , bench "lookupCrt 6623" $ nf lookupCrt 6623
-      , bench "lookupIndex (0,0)" $ nf (lookupIndex 0 0) ()
-      , bench "lookupIndex (143,45)" $ nf (lookupIndex 143 45) ()
-      ]
-  , bgroup "Trit"
+  [ bgroup "Trit"
       [ bench "add N Z" $ nf (add N) Z
-      , bench "mul P P" $ nf (mul P P) ()
+      , bench "mul P P" $ whnf (mul P) P
       , bench "superpose N P" $ nf (superpose N) P
       ]
   , bgroup "Tryte"
-      [ bench "decode 364" $ nf (length . decode . mkTryte) 364
+      [ bench "length . decode . mkTryte" $ nf (length . decode . mkTryte) (365 :: Word8)
       ]
   ]
