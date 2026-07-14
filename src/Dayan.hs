699c5865e81bc7a9b2da752993cb6f4536b61c81
Yan Li <yanli@trit.local>
2026-07-14 19:21:38 +0800

    feat: Dayan 顶层重导出模块 + API 对齐
    - src/Dayan.hs: 统一入口, 重导出 Core/Compute/ProofGen
    - 排除冲突名称 (isValid/polarWinding/等)
    - dype.cabal: 添加 Dayan 到 exposed-modules

---
+++ src/Dayan.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Dayan
+  ( module Dayan.Core.Trit
+  , module Dayan.Core.Tryte
+  , module Dayan.Core.Constants
+  , module Dayan.Compute.CRT
+  , module Dayan.Compute.ModArith
+  , module Dayan.ProofGen.AST
+  , module Dayan.ProofGen.Emit
+  , module Dayan.ProofGen.Templates
+  ) where
+
+import Dayan.Core.Trit
+import Dayan.Core.Tryte
+import Dayan.Core.Constants
+import Dayan.Compute.CRT
+import Dayan.Compute.ModArith
+import Dayan.ProofGen.AST
+import Dayan.ProofGen.Emit
+import Dayan.ProofGen.Templates
